3eef8bb593a51d020638ef6c57de28d898ef2b64
DragoRyu <51171600+Sanchith26@users.noreply.github.com>
2021-06-13 19:02:34 +0530

    Fixed Bug Multiple Triggers

--- Game Jam Submission Project/Assets/Scripts/TriggerPreset.cs
+++ Game Jam Submission Project/Assets/Scripts/TriggerPreset.cs
@@ -6,11 +6,17 @@ public class TriggerPreset : MonoBehaviour
 {
     public float difference;
     [SerializeField] Transform endPosition;
+    bool canTrigger;
+    private void Start()
+    {
+        canTrigger = true;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag=="Player")
+        if(collision.tag=="Player" && canTrigger)
         {
             LevelGenerator.Instance.GeneratePreset(endPosition.position);
+            canTrigger = false;
             Destroy(this.gameObject, 40f);
         }
     }
