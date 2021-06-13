90725951534bd51699cdd08068c2e810063f4010
ROBOK09 <70260887+ROBOK09@users.noreply.github.com>
2021-06-13 22:24:09 +0530

    Pause Menu

--- Game Jam Submission Project/Assets/Scripts/Pause_Menu.cs
+++ Game Jam Submission Project/Assets/Scripts/Pause_Menu.cs
@@ -1,42 +1,12 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;   
 
 public class Pause_Menu : MonoBehaviour
 {
-    public AudioSource audioVolume;
-    public TMP_Dropdown resolutionDropdown;
-    Resolution[] resolutions;
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
-    void Start()
-    {
-        GameIsPaused = false;
-        Time.timeScale = 1f;
-        pauseMenuUI.SetActive(false);
-
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + "X" + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
 
     void Update()
     {
@@ -67,28 +37,14 @@ public class Pause_Menu : MonoBehaviour
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
-
-    public void SetResolution(int resoltionIndex)
-    {
-        Resolution resolution = resolutions[resoltionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-    }
-    public void SetVolume(float Volume)
-    {
-        audioVolume.volume = Volume / 100;
-    }
-
-    public void SetQuality(int qualityIndex)
-    {
-        QualitySettings.SetQualityLevel(qualityIndex);
-    }
-
-    public void SetFullscreen(bool isFullScreen)
-    {
-        Screen.fullScreen = isFullScreen;
-    }
     public void BackToMainMenu()
     {
         //SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
     }
 }
