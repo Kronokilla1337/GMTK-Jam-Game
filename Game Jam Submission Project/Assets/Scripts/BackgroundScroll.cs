6b542165f0ab9c248aed6d99b435c8783d43827b
DragoRyu <51171600+Sanchith26@users.noreply.github.com>
2021-06-13 20:33:05 +0530

    BackgroundScroll

---
+++ Game Jam Submission Project/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    float textureUnitSize;
+    Transform cameraTransform;
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSize = texture.width / sprite.pixelsPerUnit;
+    }
+
+    void LateUpdate()
+    {
+        if (cameraTransform.position.x - transform.position.x >= 30)
+        {
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSize;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y, 10);
+        }
+    }
+}
