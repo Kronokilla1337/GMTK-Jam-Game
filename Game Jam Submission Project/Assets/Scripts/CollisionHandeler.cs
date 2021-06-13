fdad3a3b39c6c77c464665261a1c416cc13ec87b
DragoRyu <51171600+Sanchith26@users.noreply.github.com>
2021-06-13 14:45:55 +0530

    Added Score System to main Scene

--- Game Jam Submission Project/Assets/Scripts/CollisionHandeler.cs
+++ Game Jam Submission Project/Assets/Scripts/CollisionHandeler.cs
@@ -6,17 +6,37 @@ public class CollisionHandeler : MonoBehaviour
 {
     [SerializeField] GameObject deathScreen;
     [SerializeField] float timeOfDeathAnimation;
-
+    PlayerMovement player;
+    bool hasDied;
+    private void Start()
+    {
+        player = GetComponent<PlayerMovement>();
+        hasDied = false;
+    }
+    private void Update()
+    {
+        if(transform.position.y <= -10f && !hasDied)
+        {
+            Death();
+            hasDied = true;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Trap"))
         {
-            //Dissolve Animation
-            if (true) //animation ended
-            {
-                Time.timeScale = 0f;
-                deathScreen.SetActive(true);
-            }
+            Death();   
+        }
+    }
+    void Death()
+    {
+        //Dissolve Animation
+        if (true) //animation ended
+        {
+            Time.timeScale = 0f;
+            Destroy(this.gameObject, 5f);
+            player.MoveBool(false);
+            deathScreen.SetActive(true);
         }
     }
 }
